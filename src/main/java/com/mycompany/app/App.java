a235f94fd4cee50aed961f8c99d4083a59f222ab
Mike <mdyrbus@tucasi.com>
2018-09-05 11:22:51 +0100

    yeah

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,7 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
-        if ( false == false ){
+        if ( false == true ){
             System.out.println("oops");
         }
     }
