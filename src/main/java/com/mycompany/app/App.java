2d6d36eb1a615e513b8241912c9bdc11fc74a283
Mike <mdyrbus@tucasi.com>
2018-09-05 11:21:17 +0100

    trying to break findbugs

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,9 +12,13 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
-        if (false){
+        if ( false == false ){
             System.out.println("oops");
         }
+    }
+
+    public void missed(){
+        String.format("%d", "1");
     }
 
     private final String getMessage() {
